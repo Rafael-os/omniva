ba50634a427d71e2e4aec67cb40e2670fb0d335d
Rafael-os <osawa.rafael@gmail.com>
2022-01-15 22:36:37 +0200

    add: table on index

--- app/controllers/parcel_machines_controller.rb
+++ app/controllers/parcel_machines_controller.rb
@@ -3,7 +3,9 @@ class ParcelMachinesController < ApplicationController
   def index
     json = REDIS.get "omniva"
 
-    @parcel_machines = JSON.parse(json)
+    parsed_response = JSON.parse(json)
+
+    @parcel_machines = ParcelMachinesDecorator.decorate_collection(parsed_response)
   end
 
   def show
---
+++ app/decorators/parcel_machines_decorator.rb
@@ -0,0 +1,15 @@
+class ParcelMachinesDecorator < ApplicationDecorator
+  def full_address
+    a0 = object["A0_NAME"]
+    a1 = object["A1_NAME"]
+    a2 = object["A2_NAME"]
+    a3 = object["A3_NAME"]
+    a4 = object["A4_NAME"]
+    a5 = object["A5_NAME"]
+    a6 = object["A6_NAME"]
+    a7 = object["A7_NAME"]
+    a8 = object["A8_NAME"]
+
+    "#{a0} #{a1} #{a2} #{a3} #{a4} #{a5} #{a6} #{a7} #{a8}"
+  end
+end
