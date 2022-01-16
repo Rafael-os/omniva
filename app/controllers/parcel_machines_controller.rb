041134403a8bad59a2e8b81872656148f90ef38c
Rafael-os <osawa.rafael@gmail.com>
2022-01-16 15:20:22 +0200

    add: table filter

--- app/controllers/parcel_machines_controller.rb
+++ app/controllers/parcel_machines_controller.rb
@@ -1,6 +1,10 @@
 class ParcelMachinesController < ApplicationController
   def index
-    @parcel_machines = ParcelMachinesDecorator.decorate_collection(parsed_response)
+    if params[:q].present?
+      @parcel_machines = search_query(params["field"])
+    else
+      @parcel_machines = ParcelMachinesDecorator.decorate_collection(parsed_response)
+    end
   end
 
   def show
@@ -15,5 +19,29 @@ class ParcelMachinesController < ApplicationController
     response = REDIS.get "omniva"
 
     JSON.parse(response)
+  end
+
+  def search_query(column)
+    if column == "zip"
+      filtered_parcel_machines = parsed_response.select{ |h| h["ZIP"].include?(params[:q]) }
+      ParcelMachinesDecorator.decorate_collection(filtered_parcel_machines)
+    end
+    if column == "name"
+      filtered_parcel_machines = parsed_response.select{ |h| h["NAME"].include?(params[:q]) }
+      ParcelMachinesDecorator.decorate_collection(filtered_parcel_machines)
+    end
+    if column == "address"
+      filtered_parcel_machines = parsed_response.select{ |h| h["A0_NAME"].include?(params[:q]) ||
+                                                             h["A1_NAME"].include?(params[:q]) ||
+                                                             h["A2_NAME"].include?(params[:q]) || 
+                                                             h["A3_NAME"].include?(params[:q]) || 
+                                                             h["A4_NAME"].include?(params[:q]) || 
+                                                             h["A5_NAME"].include?(params[:q]) || 
+                                                             h["A6_NAME"].include?(params[:q]) || 
+                                                             h["A7_NAME"].include?(params[:q]) ||
+                                                             h["A8_NAME"].include?(params[:q])
+                                                        }
+      ParcelMachinesDecorator.decorate_collection(filtered_parcel_machines)
+    end
   end
 end
--- app/models/parcel_machine.rb
+++ app/models/parcel_machine.rb
@@ -1,7 +1,3 @@
 class ParcelMachine < ApplicationRecord
-  def self.store_json 
-    response = Faraday.get('https://www.omniva.lt/locations.json')
 
-    REDIS.set "omniva", response.body
-  end
 end
